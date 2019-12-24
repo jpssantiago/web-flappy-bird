5c4ac9d4c622617cab2cb23775daa4bfcebb7ebe
João Pedro Santiago <joaopedrosantiago@macbookjp.local>
2019-12-24 17:43:57 -0200

    Added basic and non-optimized collision.

--- js/game.js
+++ js/game.js
@@ -4,6 +4,29 @@ function newElement(tagName, className){
     return element
 }
 
+function elementsOverlapping(elementA, elementB){
+    const a = elementA.getBoundingClientRect()
+    const b = elementB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width > b.left && b.left + b.width > a.left
+    const vertical = a.top + a.height - 15 >= b.top && b.top + b.height >= a.top
+
+    return horizontal && vertical
+}
+
+function checkCollision(bird, barriers){
+    let collided = false
+    barriers.pairs.forEach(pair => {
+        if(!collided){
+            const upper = pair.higher.element
+            const lower = pair.lower.element
+
+            collided = elementsOverlapping(bird.element, upper) || elementsOverlapping(bird.element, lower)
+        }
+    })
+    return collided
+}
+
 function Game(){
     let points = 0
 
@@ -20,9 +43,14 @@ function Game(){
     gameArea.appendChild(bird.element)
 
     this.start = () => {
-        setInterval(() => {
+        const timer = setInterval(() => {
             barriers.animate()
             bird.animate()
+
+            if(checkCollision(bird, barriers)){
+                clearInterval(timer)
+                console.log('Collision detected. Game over!')
+            }
         }, 20)
     }
 }
